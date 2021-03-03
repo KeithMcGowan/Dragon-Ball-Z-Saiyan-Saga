2dc5d268dbe72cafc1d4b2667cffd02683b09c7f
Keith <keithrichardmcgowan@gmail.com>
2021-03-02 19:51:59 -0500

    fix life and saibamen killed

--- Game.js
+++ Game.js
@@ -13,7 +13,7 @@ class Game {
     this.level = new Level();
     this.saibamen = new Saibamen();
     this.collision = false;
-    this.health = 1;
+    this.health = 10;
     this.saibamenKilled = 9;
     this.gameLoop();
   }
@@ -35,24 +35,32 @@ class Game {
 
   checkForDefeat() {
     if (this.health <= 0) {
-      ctx.font = '60px Comic Sans MS';
-      ctx.strokeStyle = 'black';
+      ctx.font = "60px Comic Sans MS";
+      ctx.strokeStyle = "black";
       ctx.lineWidth = 8;
-      ctx.strokeText("Game Over!", (canvas.width/2 - 160), canvas.height/2);
-      ctx.fillStyle = '#e65c00';
-      ctx.fillText("Game Over!", (canvas.width/2 - 160), canvas.height/2);
+      ctx.strokeText("Game Over!", canvas.width / 2 - 160, canvas.height / 2);
+      ctx.fillStyle = "#e65c00";
+      ctx.fillText("Game Over!", canvas.width / 2 - 160, canvas.height / 2);
       clearInterval(this.gameID());
     }
   }
 
   checkForVictory() {
     if (this.saibamenKilled >= 10) {
-      ctx.font = '60px Comic Sans MS';
-      ctx.strokeStyle = 'black';
+      ctx.font = "60px Comic Sans MS";
+      ctx.strokeStyle = "black";
       ctx.lineWidth = 8;
-      ctx.strokeText("Earth is Saved!", (canvas.width/2 - 200), canvas.height/2);
-      ctx.fillStyle = '#e65c00';
-      ctx.fillText("Earth is Saved!", (canvas.width/2 - 200), canvas.height/2);
+      ctx.strokeText(
+        "Earth is Saved!",
+        canvas.width / 2 - 200,
+        canvas.height / 2
+      );
+      ctx.fillStyle = "#e65c00";
+      ctx.fillText(
+        "Earth is Saved!",
+        canvas.width / 2 - 200,
+        canvas.height / 2
+      );
       clearInterval(this.gameID());
     }
   }
@@ -60,9 +68,9 @@ class Game {
   detectCollisions(obj1, obj2) {
     return !(
       obj1.y > obj2.y + obj2.height ||
-      obj1.x > obj2.x + obj2.width-20 ||
+      obj1.x > obj2.x + obj2.width - 20 ||
       obj1.y + obj1.height < obj2.y ||
-      obj1.x + obj1.width-20 < obj2.x
+      obj1.x + obj1.width - 20 < obj2.x
     );
   }
 
@@ -70,39 +78,49 @@ class Game {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     this.level.drawLevel(this.level.background0);
-    
+
     this.checkForVictory();
 
     this.saibamenArrayLeft.forEach((eachSaibamen, index) => {
       eachSaibamen.drawEnemy();
-      if (!this.goku.recentlyHit && this.detectCollisions(this.goku, eachSaibamen)) {
+      if (
+        !this.goku.recentlyHit &&
+        this.detectCollisions(this.goku, eachSaibamen)
+      ) {
         this.health -= 1;
         document.querySelector("#health span").innerHTML = this.health;
         console.log("collision");
         this.goku.recentlyHit = true;
         setTimeout(() => {
-            this.goku.recentlyHit = false;
+          this.goku.recentlyHit = false;
         }, 1000);
       }
       if (this.goku.attack !== false) {
-          const hitbox = {x: this.goku.attack, y: this.goku.y, height: this.goku.height, width: this.goku.width}
-          if (this.detectCollisions(hitbox, eachSaibamen)) {
-            this.saibamenKilled += 1;
-            document.querySelector('#killed span').innerHTML = this.saibamenKilled;
-            console.log('hit')
-            this.saibamenArrayLeft.splice(index, 1);
-            this.goku.attack = false;
-          }
+        const hitbox = {
+          x: this.goku.attack,
+          y: this.goku.y,
+          height: this.goku.height,
+          width: this.goku.width,
+        };
+        if (this.detectCollisions(hitbox, eachSaibamen)) {
+          this.saibamenKilled += 1;
+          document.querySelector(
+            "#killed span"
+          ).innerHTML = this.saibamenKilled;
+          console.log("hit");
+          this.saibamenArrayLeft.splice(index, 1);
+          this.goku.attack = false;
         }
-      });
-      
-      this.goku.drawGoku();
-      
-      this.goku.move(this);
-      
-      let randoLeft = Math.random();
-      if (randoLeft > 0.998) this.generateSaibamenLeft();
-      
+      }
+    });
+
+    this.goku.drawGoku();
+
+    this.goku.move(this);
+
+    let randoLeft = Math.random();
+    if (randoLeft > 0.998) this.generateSaibamenLeft();
+
     this.checkForDefeat();
 
     this.gameID = requestAnimationFrame(this.gameLoop);
